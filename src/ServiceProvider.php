57679bf9534031d6acbc48051bb9dd23a9d6761b
Vladimir <vladimirpeshkov5dev@yandex.ru>
2020-08-18 10:34:32 +0300

    initial commit

---
+++ src/ServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace PortedCheese\ProductVariation;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+
+  public function boot()
+  {
+    // code...
+  }
+
+  public function register()
+  {
+    // code...
+  }
+}
